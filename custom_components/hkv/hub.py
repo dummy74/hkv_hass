5c5aaeb07a0df9ed8dc85543e8b85336e0e62d5c
Holger Priwitzer <holger.priwitzer@eas.iis.fraunhofer.de>
2023-01-31 01:09:10 +0100

    testing

--- custom_components/hkv/hub.py
+++ custom_components/hkv/hub.py
@@ -59,11 +59,11 @@ class HKVHub:
         _LOGGER.info(f"{addr=}: {devices[addr]=}")
         return {"devices": devices}
         
-    async def fetch_data(self):
+    async def fetch_data(self, hass):
                         
         devices = OrderedDict()
         def defaultdevdata():
-            return dict(
+            return OrderedDict(
                 ID='UNKNOWN',
                 #DISPLAY=dict(USED=False, READY=False),
                 #USB=dict(USED=False, READY=False),
@@ -80,44 +80,33 @@ class HKVHub:
         dev = defaultdevdata()
         #dev = {'temp_transmit_interval':30000,'temp_measure_interval':30000,'CNT':None}
         
-        _,state_pck = self.hkv.get_status(dst=0,timeout=60)
-        _LOGGER.warning(f"0:{state_pck=}")
+        state_pck = None; retry = 5
+        while state_pck is None and retry:
+            retry -= 1
+            _,state_pck = await hass.async_add_executor_job(self.hkv.get_status(dst=0,timeout=60))
+            _LOGGER.warning(f"0:{state_pck=}")
         if state_pck is not None:
-            dev['ID'] = state_pck.ID
-            dev['DISPLAY'] = state_pck.DISPLAY
-            dev['USB'] = state_pck.USB
-            dev['LORA'] = state_pck.LORA
-            dev['RELAIS'] = state_pck.RELAIS
-            dev['SENSOR'] = state_pck.SENSOR
+            dev.update(asdict(state_pck))
         
-        _,temps_pck = self.hkv.get_temps(dst=0,timeout=60)
-        _LOGGER.warning(f"0:{temps_pck=}")
+        temps_pck = None; retry = 5
+        while temps_pck is None and retry:
+            retry -= 1
+            _,temps_pck = await hass.async_add_executor_job(self.hkv.get_temps(dst=0,timeout=60))
+            _LOGGER.warning(f"0:{temps_pck=}")
         if temps_pck is not None:
-            dev['CNT'] = temps_pck.CNT
-            devices[temps_pck.SRC] = dev
-            num = state_pck.SENSOR['NUM']
-            for ii in range(num):
-                tempi = f"Temp{ii+1}"
-                temp = getattr(temps_pck,tempi,None)
-                if temp:
-                    dev[tempi] = temp
-                else:
-                    _LOGGER.error(f"{tempi} not present")
-                    continue
-        _,relais_pck = self.hkv.get_relais(dst=0,timeout=60)
-        _LOGGER.warning(f"0:{relais_pck}")
+            dev.update(asdict(temps_pck))
+        relais_pck = None; retry = 5
+        while relais_pck is None and retry:
+            retry -= 1
+            _,relais_pck = await hass.async_add_executor_job(self.hkv.get_relais(dst=0,timeout=60))
+            _LOGGER.warning(f"0:{relais_pck=}")
         if relais_pck is not None:
-        #dev['ID'] = relais_pck.ID
-            for ii in range(6):
-                reli = f"Relais{ii+1}"
-                rel = getattr(relais_pck,reli,None)
-                if not rel is None:
-                    dev[reli] = rel
-                else:
-                    _LOGGER.error(f"{reli} not present")
-                    continue
-        _,conn_pck = self.hkv.get_connections(dst=0,timeout=60) # HKV-Base
-        _LOGGER.warning(f"{conn_pck=}")
+            dev.update(asdict(relais_pck))
+        conn_pck = None; retry = 5
+        while conn_pck is None and retry:
+            retry -= 1
+            _,conn_pck = await hass.async_add_executor_job(self.hkv.get_connections(dst=0,timeout=60)) # HKV-Base
+            _LOGGER.warning(f"{conn_pck=}")
         if conn_pck:
             try:
                 for i in range(10):
@@ -130,49 +119,26 @@ class HKVHub:
                         state_pck = None; retry = 5
                         while state_pck is None and retry:
                             retry -= 1
-                            _,state_pck = self.hkv.get_status(dst=addr,timeout=10)
-                        _LOGGER.warning(f"{addr}:{retry=}:{state_pck=}")
+                            _,state_pck = await hass.async_add_executor_job(self.hkv.get_status(dst=addr,timeout=10))
+                            _LOGGER.warning(f"{addr}:{retry=}:{state_pck=}")
                         if state_pck is not None:
-                            dev['ID'] = state_pck.ID
-                            dev['DISPLAY'] = state_pck.DISPLAY
-                            dev['USB'] = state_pck.USB
-                            dev['LORA'] = state_pck.LORA
-                            dev['RELAIS'] = state_pck.RELAIS
-                            dev['SENSOR'] = state_pck.SENSOR
+                            dev.update(asdict(state_pck))
                         
                         temps_pck = None; retry = 5
                         while temps_pck is None and retry:
                             retry -= 1
-                            _,temps_pck = self.hkv.get_temps(dst=addr,timeout=10)
+                            _,temps_pck = await hass.async_add_executor_job(self.hkv.get_temps(dst=addr,timeout=10))
                             _LOGGER.warning(f"{addr}:{retry=}:{temps_pck}")
                         if temps_pck is not None:
-                            dev['ID'] = temps_pck.ID
-                            dev['CNT'] = temps_pck.CNT
-                            num = state_pck.SENSOR['NUM']
-                            for ii in range(num):
-                                tempi = f"Temp{ii+1}"
-                                temp = getattr(temps_pck,tempi,None)
-                                if temp:
-                                    dev[tempi] = temp
-                                else:
-                                    _LOGGER.error(f"{tempi} not present")
-                                    continue
+                            dev.update(asdict(temps_pck))
                         
                         relais_pck = None; retry = 5
                         while relais_pck is None and retry:
                             retry -= 1
-                            _,relais_pck = self.hkv.get_relais(dst=addr,timeout=10)
-                        _LOGGER.warning(f"{addr}:{retry=}:{relais_pck}")
+                            _,relais_pck = await hass.async_add_executor_job(self.hkv.get_relais(dst=addr,timeout=10))
+                            _LOGGER.warning(f"{addr}:{retry=}:{relais_pck}")
                         if relais_pck is not None:
-                            #dev['ID'] = relais_pck.ID
-                            for ii in range(6):
-                                reli = f"Relais{ii+1}"
-                                rel = getattr(relais_pck,reli,None)
-                                if not rel is None:
-                                    dev[reli] = rel
-                                else:
-                                    _LOGGER.error(f"{reli} not present")
-                                    continue
+                            dev.update(asdict(relais_pck))
             except: pass
             
         #_LOGGER.warning(f"set temps measure interval: {self.hkv.set_temps_measure_period(delay=1000, period=30000, dst=-1, timeout=20)}")
