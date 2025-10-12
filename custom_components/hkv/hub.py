5384eb055aa5f240724110c5f9623d85090af0ef
dummy <dummy74@web.de>
2025-10-12 19:55:35 +0200

    change: serial instead of parallel execution of requests (TESTING)

--- custom_components/hkv/hub.py
+++ custom_components/hkv/hub.py
@@ -61,20 +61,25 @@ class HKVHub:
 
         try:
             dev = defaultdevdata()
-            # Parallel queries for base device (dst=0)
+            # # Parallel queries for base device (dst=0)
+            # tasks = [
+            #     self.hkv.get_status(dst=0, timeout=5),
+            #     self.hkv.get_temps(dst=0, timeout=5),
+            #     self.hkv.get_relais(dst=0, timeout=5),
+            #     self.hkv.get_connections(dst=0, timeout=5)
+            # ]
+            # results = await asyncio.gather(*tasks, return_exceptions=True)
+
+            # state_pck = results[0][1] if isinstance(results[0], tuple) and results[0][0] else None
+            # temps_pck = results[1][1] if isinstance(results[1], tuple) and results[1][0] else None
+            # relais_pck = results[2][1] if isinstance(results[2], tuple) and results[2][0] else None
+            # conn_pck = results[3][1] if isinstance(results[3], tuple) and results[3][0] else None
+
             tasks = [
-                self.hkv.get_status(dst=0, timeout=5),
-                self.hkv.get_temps(dst=0, timeout=5),
-                self.hkv.get_relais(dst=0, timeout=5),
-                self.hkv.get_connections(dst=0, timeout=5)
+                self.hkv.get_status(dst=0, timeout=10),
             ]
             results = await asyncio.gather(*tasks, return_exceptions=True)
-
             state_pck = results[0][1] if isinstance(results[0], tuple) and results[0][0] else None
-            temps_pck = results[1][1] if isinstance(results[1], tuple) and results[1][0] else None
-            relais_pck = results[2][1] if isinstance(results[2], tuple) and results[2][0] else None
-            conn_pck = results[3][1] if isinstance(results[3], tuple) and results[3][0] else None
-
             if state_pck:
                 devices[state_pck.SRC] = dev
                 dev['ID'] = state_pck.ID
@@ -84,6 +89,11 @@ class HKVHub:
                 dev['RNUM'] = state_pck.RNUM
                 dev['CCNT'] = state_pck.CCNT
 
+            tasks = [
+                self.hkv.get_temps(dst=0, timeout=10),
+            ]
+            results = await asyncio.gather(*tasks, return_exceptions=True)
+            temps_pck = results[0][1] if isinstance(results[0], tuple) and results[0][0] else None
             if temps_pck:
                 dev['SNUM'] = temps_pck.SNUM
                 dev['MCNT'] = temps_pck.MCNT
@@ -93,6 +103,11 @@ class HKVHub:
                     temp = dev['TDATA'][ii]
                     dev[tempi] = temp if temp else None
 
+            tasks = [
+                self.hkv.get_relais(dst=0, timeout=10),
+            ]
+            results = await asyncio.gather(*tasks, return_exceptions=True)
+            relais_pck = results[0][1] if isinstance(results[0], tuple) and results[0][0] else None
             if relais_pck:
                 dev['RNUM'] = relais_pck.RNUM
                 dev['RDATA'] = relais_pck.RDATA
@@ -101,6 +116,11 @@ class HKVHub:
                     rel = dev['RDATA'][ii]
                     dev[reli] = rel if rel is not None else None
 
+            tasks = [
+                self.hkv.get_connections(dst=0, timeout=10)
+            ]
+            results = await asyncio.gather(*tasks, return_exceptions=True)
+            conn_pck = results[0][1] if isinstance(results[0], tuple) and results[0][0] else None
             # Handle connections
             if conn_pck:
                 query_tasks = []
@@ -125,17 +145,23 @@ class HKVHub:
         return {"devices": devices}
 
     async def _query_device(self, addr, dev):
+        # tasks = [
+        #     self.hkv.get_status(dst=addr, timeout=5),
+        #     self.hkv.get_temps(dst=addr, timeout=5),
+        #     self.hkv.get_relais(dst=addr, timeout=5)
+        # ]
+        # results = await asyncio.gather(*tasks, return_exceptions=True)
+
+        # state_pck = results[0][1] if isinstance(results[0], tuple) and results[0][0] else None
+        # temps_pck = results[1][1] if isinstance(results[1], tuple) and results[1][0] else None
+        # relais_pck = results[2][1] if isinstance(results[2], tuple) and results[2][0] else None
+
         tasks = [
-            self.hkv.get_status(dst=addr, timeout=5),
-            self.hkv.get_temps(dst=addr, timeout=5),
-            self.hkv.get_relais(dst=addr, timeout=5)
+            self.hkv.get_status(dst=addr, timeout=10),
         ]
         results = await asyncio.gather(*tasks, return_exceptions=True)
 
         state_pck = results[0][1] if isinstance(results[0], tuple) and results[0][0] else None
-        temps_pck = results[1][1] if isinstance(results[1], tuple) and results[1][0] else None
-        relais_pck = results[2][1] if isinstance(results[2], tuple) and results[2][0] else None
-
         if state_pck:
             dev['ID'] = state_pck.ID
             dev['MSEC'] = state_pck.MSEC
@@ -144,6 +170,12 @@ class HKVHub:
             dev['RNUM'] = state_pck.RNUM
             dev['CCNT'] = state_pck.CCNT
 
+        tasks = [
+            self.hkv.get_temps(dst=addr, timeout=10),
+        ]
+        results = await asyncio.gather(*tasks, return_exceptions=True)
+
+        temps_pck = results[0][1] if isinstance(results[0], tuple) and results[0][0] else None
         if temps_pck:
             dev['SNUM'] = temps_pck.SNUM
             dev['MCNT'] = temps_pck.MCNT
@@ -153,6 +185,12 @@ class HKVHub:
                 temp = dev['TDATA'][ii]
                 dev[tempi] = temp if temp else None
 
+        tasks = [
+            self.hkv.get_relais(dst=addr, timeout=10)
+        ]
+        results = await asyncio.gather(*tasks, return_exceptions=True)
+
+        relais_pck = results[0][1] if isinstance(results[0], tuple) and results[0][0] else None
         if relais_pck:
             dev['RNUM'] = relais_pck.RNUM
             dev['RDATA'] = relais_pck.RDATA
